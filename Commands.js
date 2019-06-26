22e76026a0cb6a96521d89ec392a6a8dfa342495
DeveloperJosh <39308600+DeveloperJosh@users.noreply.github.com>
2019-06-26 09:13:59 -0400

    Update Commands.js

--- Commands.js
+++ Commands.js
@@ -1,6 +1,8 @@
-/This a test so cat sit your ass down
-if ! + !! !Help()
-bot.log(0)
-message.send()
-console.log error with !Help Cat
-/cat what did you do???
+//This a test so cat sit your ass down
+'if ! + !! !Help()'
+'bot.log(0)'
+'message.send()'
+'console.log error with !Help Cat'
+//cat what did you do???
+'message.send' "testing 1.2.3"
+//Testing this sooo reee
